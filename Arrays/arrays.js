46a47d3ca301fcf6fb0e91e26d4befd4a19d97ff
Your NameChristian <christianhermanndev@gmail.com>
2025-09-18 14:21:47 -0500

    Nested Arrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -41,3 +41,29 @@ const pastaIndex = groceryList.indexOf("pasta");
 console.log(pastaIndex);
 
 // Arrays and Functions
+const concept = ["arrays", "can", "be", "mutated"];
+
+function changeArr(arr) {
+  arr[3] = "MUTATED";
+}
+
+changeArr(concept);
+
+console.log(concept);
+
+const removeElement = (newArr) => {
+  newArr.pop();
+};
+
+removeElement(concept);
+
+console.log(concept);
+
+// Nested arrays
+const numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+const target = numberClusters[2][1]; // target 6
