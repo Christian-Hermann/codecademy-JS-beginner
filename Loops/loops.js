77d45c9dc5e21c30dcbfb3fb04d91081dd001ffb
Your NameChristian <christianhermanndev@gmail.com>
2025-09-18 18:55:22 -0500

    The while loop

--- Loops/loops.js
+++ Loops/loops.js
@@ -9,3 +9,22 @@ for (let i = 3; i >= 0; i--) {
 }
 
 // Looping through arrays
+const vacationSpots = ["Bali", "Paris", "Tulum"];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log("I would love to visit " + vacationSpots[i]);
+}
+
+// Nested loops
+const bobsFollowers = ["Kit", "Hermann", "Lisa", "Spampinato"];
+const tinasFollowers = ["Kit", "Lisa", "Dreyfus"];
+const mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
