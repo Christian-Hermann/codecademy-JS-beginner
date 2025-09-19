35bf59db484ccc83483d7a8b2dee369e09bd7a07
Your NameChristian <christianhermanndev@gmail.com>
2025-09-18 22:24:36 -0500

    Finished Whale Talk project

--- Loops/loops.js
+++ Loops/loops.js
@@ -28,3 +28,36 @@ for (let i = 0; i < bobsFollowers.length; i++) {
     }
   }
 }
+
+// The While Loop
+const cards = ["diamond", "spade", "heart", "club"];
+
+// Write your code below
+let currentCard;
+
+while (currentCard !== "spade") {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
+
+// Do...While Statements
+let cupsOfSugarNeeded = 10;
+let cupsAdded = 0;
+
+do {
+  cupsAdded += 1;
+  console.log(cupsAdded);
+} while (cupsAdded < cupsOfSugarNeeded);
+
+// The break Keyword
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === "Notorious B.I.G.") {
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
