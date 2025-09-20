eae75c965373c1d4ed1d7131b35297f5acb5ba1a
Your NameChristian <christianhermanndev@gmail.com>
2025-09-20 18:10:56 -0500

    Iterator doumentation

--- Iterators/iterators.js
+++ Iterators/iterators.js
@@ -106,3 +106,14 @@ const startsWithS = animals.findIndex((item) => {
 // console.log(animals[3])
 
 // The .reduce() Method
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log("The value of accumulator: ", accumulator);
+  console.log("The value of currentValue: ", currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
+
+// Iterator Documentation
