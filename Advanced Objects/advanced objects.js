53cedc987ba74574570ec7465260e071a63a7de6
Your NameChristian <christianhermanndev@gmail.com>
2025-09-23 15:04:48 -0500

    Getters

--- Advanced Objects/advanced objects.js
+++ Advanced Objects/advanced objects.js
@@ -8,3 +8,18 @@ const robot = {
 };
 
 console.log(robot.provideInfo());
+
+// Privacy
+const robot = {
+  _energyLevel: 100,
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+  },
+};
+
+robot._energyLevel = "high";
+
+console.log(robot.recharge());
+
+// Getters
