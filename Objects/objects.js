6c7dac3d668fdfd10ac3663240ab2e3550b113e5
Your NameChristian <christianhermanndev@gmail.com>
2025-09-23 14:29:52 -0500

    The this keyword

--- Objects/objects.js
+++ Objects/objects.js
@@ -80,3 +80,21 @@ spaceship.passengers = [(Kit = { pet: true }), (Lisa = { pet: false })];
 firstPassenger = spaceship.passengers[0];
 
 // Pass by reference
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+};
+
+// Write your code below
+
+let greenEnergy = (obj) => {
+  obj["Fuel Type"] = "avocado oil";
+};
+
+let remotelyDisable = (obj) => {
+  obj.disabled = true;
+};
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);
