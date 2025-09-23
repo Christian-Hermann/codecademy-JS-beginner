74eff92320b3f4141c06a2e2887a4c94c61e1c8d
Your NameChristian <christianhermanndev@gmail.com>
2025-09-22 19:06:22 -0500

    Pass by reference

--- Objects/objects.js
+++ Objects/objects.js
@@ -24,3 +24,59 @@ spaceship.color = "glorious gold";
 spaceship.numEngines = 8;
 
 delete spaceship["Secret Mission"];
+
+// Methods
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+
+alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+alienShip.retreat();
+alienShip.takeOff();
+
+// Nested Objects
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+
+let capFave = spaceship.crew.captain["favorite foods"][0];
+
+spaceship.passengers = [(Kit = { pet: true }), (Lisa = { pet: false })];
+
+firstPassenger = spaceship.passengers[0];
+
+// Pass by reference
