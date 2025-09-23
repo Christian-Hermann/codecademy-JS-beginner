92fa0b3e2382f85b3fff507bf3ad412bf4445210
Your NameChristian <christianhermanndev@gmail.com>
2025-09-23 16:08:08 -0500

    Factory functions

--- Advanced Objects/advanced objects.js
+++ Advanced Objects/advanced objects.js
@@ -23,3 +23,55 @@ robot._energyLevel = "high";
 console.log(robot.recharge());
 
 // Getters
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number") {
+      return `My current energy level is ${this._energyLevel}`;
+    } else {
+      return "System malfunction: cannot retrieve energy level";
+    }
+  },
+};
+
+console.log(robot.energyLevel);
+
+// Setters
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors() {
+    if (typeof this._numOfSensors === "number") {
+      return this._numOfSensors;
+    } else {
+      return "Sensors are currently down.";
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0");
+    }
+  },
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+// Factory functions
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+
+console.log(tinCan);
